2b300e0820aa9410e1fa24cd740bf75269af4b23
louity <louis.thiry@outlook.fr>
2019-11-16 18:52:16 +0100

    add optional mask to limit the strokes

--- utils_display.py
+++ utils_display.py
@@ -5,7 +5,7 @@ from skimage.transform import resize
 import subprocess
 
 
-def save_stroke_image(strokes, xlim, ylim, aspect, screen_height, screen_width, image_y, image_x, image_height, image_width, stroke_width=1., filename='to_project.jpg'):
+def save_stroke_image(strokes, xlim, ylim, aspect, screen_height, screen_width, image_y, image_x, image_height, image_width, stroke_width=1., filename='to_project.jpg', mask=None):
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
     ax.set_xlim(xlim)
@@ -18,9 +18,11 @@ def save_stroke_image(strokes, xlim, ylim, aspect, screen_height, screen_width, 
 
     fig.savefig('tmp.jpg', bbox_inches='tight', pad_inches=0, dpi=500)
     img = np.array(PIL.Image.open('./tmp.jpg'))
-    resized_img = resize(img, (image_height, image_width))
+    resized_img = resize(img, (image_height, image_width), preserve_range=True)
+    if mask is not None:
+        resized_img *= resize(mask, (image_height, image_width), preserve_range=True)
     full_img = 255*np.ones((screen_height, screen_width, 3), dtype=np.uint8)
-    full_img[image_y:image_y+image_height, image_x:image_x+image_width,:] = 255 * resized_img
+    full_img[image_y:image_y+image_height, image_x:image_x+image_width,:] = resized_img
     im = PIL.Image.fromarray(full_img)
     im.save(filename)
 
